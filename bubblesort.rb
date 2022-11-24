37a39d5d4d583eb8c6c875d2a471618c5f08be0f
jsun3886 <jsunday86@gmail.com>
2022-11-23 20:17:06 -0500

    Implement the bubblesort algorithm
    Implement the actual bubblesort algrorithm in bubblesort.rb
    A while loop is executed as long as there are any swaps as you
    traverse the array.

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,5 +1,18 @@
 def bubblesort(arr)
-
+    unsorted = true
+    while unsorted do
+        unsorted=false
+        arr.each_with_index do |a,i|
+            if(i==arr.length-1)
+                break
+            end
+            if(a > arr[i+1])
+                unsorted=true
+                arr[i], arr[i+1]=arr[i+1],arr[i]
+            end    
+            puts "#{arr} is the current array"
+        end
+    end
 
     return arr
 end
@@ -7,4 +20,4 @@ end
 
 
 
-puts bubble_sort([4,3,78,2,0,2])
+puts "#{bubblesort([4,3,78,2,0,2])} is sorted"
